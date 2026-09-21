4a380aa335f75d4fea693de743aaa21a1db05361
fclivaz42 <fclivaz@pm.me>
2026-09-21 03:28:06 +0200

    fix strlen

--- srcs/base/ft_strlen.s
+++ srcs/base/ft_strlen.s
@@ -6,25 +6,26 @@
 ;    By: fclivaz <fclivaz@student.42lausanne.ch>    +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2026/01/09 17:16:17 by fclivaz           #+#    #+#              ;
-;    Updated: 2026/01/22 16:18:03 by fclivaz          ###   LAUSANNE.ch        ;
+;    Updated: 2026/09/21 02:21:13 by fclivaz          ###   LAUSANNE.ch        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
 section .text
-	global ft_strlen
+global ft_strlen
 
 ft_strlen:
 	push	rbp
 	mov		rbp, rsp
-	mov		rbx, rdi
-	cmp 	BYTE [rbx], 0
-	je		.end
+	mov		rax, rdi
+	mov		bl, [rax]
+	test	bl, bl
+	jz		.end
 .loop:
-	inc		rbx
-	cmp 	BYTE [rbx], 0
-	jne		.loop
+	inc		rax
+	mov		bl, [rax]
+	test	bl, bl
+	jnz		.loop
 .end:
-	sub		rbx, rdi
-	mov		rax, rbx
+	sub		rax, rdi
 	leave
 	ret
