80e1420e8411320ab5690635111e56d324e9d75a
fclivaz42 <fclivaz@pm.me>
2026-09-21 17:02:36 +0200

    ft_read finished

--- srcs/base/ft_read.s
+++ srcs/base/ft_read.s
@@ -6,13 +6,32 @@
 ;    By: fclivaz <fclivaz@student.42lausanne.ch>    +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2026/01/09 18:50:36 by fclivaz           #+#    #+#              ;
-;    Updated: 2026/01/09 18:50:38 by fclivaz          ###   LAUSANNE.ch        ;
+;    Updated: 2026/09/21 16:55:35 by fclivaz          ###   LAUSANNE.ch        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
+extern __errno_location
+global ft_read
+default rel
+
+section .data
+	sys_read dd	0x0
+
+section .text
 ft_read:
 	push	rbp
 	mov		rbp, rsp
-
+	mov		rax, [sys_read]
+	syscall
+	cmp rax, 0
+	jl .err
+	leave
+	ret
+.err:
+	neg rax
+	mov rdi, rax
+	call __errno_location WRT ..plt
+	mov [rax], rdi
+	mov rax, -1
 	leave
 	ret
