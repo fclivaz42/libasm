e719ac9b4bfdd86222f5dd3daa1f6de91fe3a4be
fclivaz42 <fclivaz@pm.me>
2026-09-22 01:14:27 +0200

    make strlen consistent with the others

--- srcs/base/ft_strlen.s
+++ srcs/base/ft_strlen.s
@@ -6,13 +6,16 @@
 ;    By: fclivaz <fclivaz@student.42lausanne.ch>    +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2026/01/09 17:16:17 by fclivaz           #+#    #+#              ;
-;    Updated: 2026/09/22 00:51:42 by fclivaz          ###   LAUSANNE.ch        ;
+;    Updated: 2026/09/22 01:14:12 by fclivaz          ###   LAUSANNE.ch        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
-section .text
 global ft_strlen
+default rel
 
+section .data
+
+section .text
 ft_strlen:
 	push	rbp
 	mov		rbp, rsp
